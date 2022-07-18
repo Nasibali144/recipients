d41fe0ca20119b161fca0217f21d714bdbd3092a
Nasibali <abdiyevnasibali@gmail.com>
2022-07-18 16:22:39 +0500

    Test Page => Add New Card

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 /// bu page faqat trim funksiyasi uchun
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: TestPage(),);
+    return const MaterialApp(home: TestPage(),);
   }
 }
 
@@ -21,37 +21,68 @@ class TestPage extends StatefulWidget {
 }
 
 class _TestPageState extends State<TestPage> {
-  TextEditingController controller = TextEditingController();
+  int counter = 0;
+  List<int> list = [];
+  ScrollController controller = ScrollController();
 
-  void onPressed() {
-    String textWithoutTrim = controller.text;
-    String textWithTrim = controller.text.trim();
-    print("textWithoutTrim: $textWithoutTrim");
-    print("textWithTrim: $textWithTrim");
+  void addNewItem() {
+    list.add(++counter);
+    setState(() {});
+    if(list.length >= 3) {
+      controller.animateTo(controller.offset + 260, duration: const Duration(milliseconds: 500), curve: Curves.linear);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                hintText: "Enter data",
+      appBar: AppBar(
+        title: Text("Learn Add Button in Bottom"),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        controller: controller,
+        padding: const EdgeInsets.all(20),
+        itemCount: list.length + 1,
+        itemBuilder: (context, index) {
+          if(index == list.length) {
+            return Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.5)
               ),
-              style: TextStyle(fontSize: 25),
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Container(
+                color: Colors.primaries[(list.length - index) % 18],
+                height: 250,
+                width: double.infinity,
+                child: Center(
+                  child: IconButton(
+                    icon: const Icon(Icons.add, size: 50,),
+                    onPressed: addNewItem,
+                  ),
+                ),
+              ),
+            );
+          }
+
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.5)
             ),
-            SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: onPressed,
-              child: Text("Press Me"),
+            margin: const EdgeInsets.only(bottom: 10),
+            child: Container(
+              color: Colors.primaries[index % 18],
+              height: 250,
+              width: double.infinity,
+              child: Center(
+                child: Text("Card: ${list[index]}", style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 25,
+                ),),
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
